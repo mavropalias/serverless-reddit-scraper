b205561ace6be3458beb082bd62ec1d0fdc1e5ed
Kostas Mavropalias <mavropalias@gmail.com>
2018-06-28 13:54:57 +0100

    basic functionality to fetch old posts

--- README.md
+++ README.md
@@ -15,6 +15,21 @@ CLIENT_ID=***
 CLIENT_SECRET=***
 REDDIT_USER=***
 REDDIT_PASS=***
+SUBREDDIT=***
+FLAIR_TEXT=***
+AWS_PROFILE=***
+```
+
+`AWS_PROFILE` refers to the corresponding section in your ~/.aws/credentials file. For example:
+
+```
+[default] ; default profile
+aws_access_key_id = <DEFAULT_ACCESS_KEY_ID>
+aws_secret_access_key = <DEFAULT_SECRET_ACCESS_KEY>
+
+[personal-account] ; personal account profile
+aws_access_key_id = <PERSONAL_ACCESS_KEY_ID>
+aws_secret_access_key = <PERSONAL_SECRET_ACCESS_KEY>
 ```
 
 ## Usage
---
+++ src/index.ts
@@ -0,0 +1,94 @@
+import snoowrap, { Listing, Submission } from 'snoowrap';
+import dotenv from 'dotenv';
+import AWS from 'aws-sdk';
+
+// Load .env
+dotenv.config();
+
+// Configure AWS
+const credentials = new AWS.SharedIniFileCredentials({
+  profile: process.env.AWS_PROFILE
+});
+AWS.config.credentials = credentials;
+const docClient = new AWS.DynamoDB.DocumentClient({
+  region: process.env.AWS_REGION,
+  endpoint: process.env.AWS_ENDPOINT,
+  convertEmptyValues: true
+});
+
+// Configure snoowrap
+const r = new snoowrap({
+  userAgent: 'reddit-scraper',
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  username: process.env.REDDIT_USER,
+  password: process.env.REDDIT_PASS
+});
+
+class RedditScraper {
+  fetchedPosts: number = 0;
+  successfullyPut: number = 0;
+  failed: number = 0;
+
+  getOldPosts(): void {
+    r.getTop(process.env.SUBREDDIT)
+      .fetchAll()
+      .then(this.processListing.bind(this));
+  }
+
+  processListing(listing: Listing<Submission>): void {
+    console.log(`- Fetched ${listing.length} posts`);
+    this.processPosts(listing);
+  }
+
+  processPosts(postsListing: Listing<Submission>): void {
+    const posts = postsListing.filter((post) => {
+      if (typeof post.secure_media_embed.content !== 'string') return false;
+      if (
+        process.env.FLAIR_TEXT &&
+        post.link_flair_text !== process.env.FLAIR_TEXT
+      ) {
+        return false;
+      }
+      return true;
+    });
+    posts.forEach((post) => this.savePost(post));
+
+    this.fetchedPosts += posts.length;
+    console.log(`- Accepted ${this.fetchedPosts} posts`);
+  }
+
+  savePost(post: Submission): void {
+    const params = {
+      TableName: 'Clips',
+      Item: {
+        id: post.id,
+        created_utc: post.created_utc,
+        title: post.title,
+        author: post.author.name,
+        permalink: post.permalink,
+        url: post.url,
+        thumbnail: post.thumbnail,
+        embed: post.secure_media_embed
+      }
+      // If you want to save the whole post, use the following:
+      // Item: post
+    };
+
+    docClient.put(params, (err, data) => {
+      if (err) {
+        //TODO: handle throttling
+        this.failed++;
+        console.error(
+          `Failed ${this.failed}/${this.fetchedPosts} (current: ${post.id}).`
+        );
+      } else {
+        this.successfullyPut++;
+        console.log(`Added ${this.successfullyPut}/${this.fetchedPosts}`);
+      }
+    });
+  }
+}
+
+const scraper = new RedditScraper();
+scraper.getOldPosts();
